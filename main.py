354d2d7b4dee1b37e897d79bc7afe471f959c1e5
MANAV-PATEL13 <manavjpatel2006@gmail.com>
2026-06-15 18:15:59 +0530

    create user task table

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from fastapi import FastAPI
+
 from src.utils.db import Base, engine
+
+# define user task table
+from src.tasks.models import TaskModel
 
 Base.metadata.create_all(engine)
 
--- src/tasks/models.py
+++ src/tasks/models.py
@@ -1,1 +1,14 @@
-# database connect
+from sqlalchemy import Column, Integer, String, Boolean
+
+# connect table with DB
+from src.utils.db import Base
+
+
+
+class TaskModel(Base):
+    __tablename__ = 'user_tasks'
+
+    id = Column( Integer, primary_key=True)
+    title = Column( String)
+    description = Column(String)
+    is_completed = Column(Boolean, default=False)
