17b744ca0bc6ec29c5d653a53bb6dbf31a3a3574
MANAV-PATEL13 <manavjpatel2006@gmail.com>
2026-06-16 00:44:07 +0530

    get all & specific task

--- main.py
+++ main.py
@@ -3,10 +3,16 @@ from fastapi import FastAPI
 from src.utils.db import Base, engine
 
 # define user task table
-from src.tasks.models import TaskModel
+# from src.tasks.models import TaskModel
+
+# connect routes with server
+from src.tasks.router import task_routes
 
 Base.metadata.create_all(engine)
 
 APP = FastAPI(
     title='Task app'
 )
+
+# task route
+APP.include_router(task_routes)
