5b7993d146a2562115bc83058020370a86100338
MANAV-PATEL13 <manavjpatel2006@gmail.com>
2026-06-22 10:59:26 +0530

    verify valid token

--- src/user/controller.py
+++ src/user/controller.py
@@ -1,4 +1,4 @@
-from fastapi import HTTPException, status
+from fastapi import HTTPException, status, Request
 from src.user.dtos import UserSchema, LoginSchema
 from sqlalchemy.orm import Session
 from src.user.models import UserModel
@@ -8,41 +8,41 @@ from src.utils.settings import settings
 from datetime import datetime, timedelta
 
 import jwt
+from jwt.exceptions import InvalidTokenError
 
 # hash
 from pwdlib import PasswordHash
 
-
 password_hash = PasswordHash.recommended()
-
 
 
 def get_password_hash(password):
     return password_hash.hash(password)
 
+
 def verify_password(plain_password, hashed_password):
     return password_hash.verify(plain_password, hashed_password)
 
 
-def register(body:UserSchema, db:Session):
+def register(body: UserSchema, db: Session):
 
     # username validation
     is_user = db.query(UserModel).filter(UserModel.username == body.username).first()
     if is_user:
-        raise HTTPException(400, detail='Username already exist.')
-    
+        raise HTTPException(400, detail="Username already exist.")
+
     # email validation
     is_user = db.query(UserModel).filter(UserModel.email == body.email).first()
     if is_user:
-        raise HTTPException(400, detail='E-mail already exist.')
+        raise HTTPException(400, detail="E-mail already exist.")
 
     hash_password = get_password_hash(body.password)
 
     new_user = UserModel(
-        name = body.name,
-        username = body.username,
-        hash_password = hash_password,
-        email = body.email
+        name=body.name,
+        username=body.username,
+        hash_password=hash_password,
+        email=body.email,
     )
 
     db.add(new_user)
@@ -52,19 +52,56 @@ def register(body:UserSchema, db:Session):
     return new_user
 
 
-def login_user(body: LoginSchema, db:Session):
-    
+def login_user(body: LoginSchema, db: Session):
+
     is_user = db.query(UserModel).filter(UserModel.username == body.username).first()
-    
+
     if not is_user:
-        raise HTTPException( status_code=status.HTTP_401_UNAUTHORIZED , detail='You enter wrong Username')
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="You enter wrong Username"
+        )
 
     if not verify_password(body.password, is_user.hash_password):
-        raise HTTPException( status_code=status.HTTP_401_UNAUTHORIZED , detail='You enter wrong Password')
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="You enter wrong Password"
+        )
 
     exp_time = datetime.now() + timedelta(minutes=settings.EXP_TIME)
+    # exp_time = datetime.now() + timedelta(seconds=30)
     print(exp_time)
 
-    token = jwt.encode({"_id":is_user.id, "exp" : exp_time}, settings.SECRET_KEY, settings.ALGORITHM)
+    token = jwt.encode(
+        {"_id": is_user.id, "exp": exp_time.timestamp()}, settings.SECRET_KEY, settings.ALGORITHM
+    )
 
-    return {"token" : token }
+    return {"token": token}
+
+
+# token send
+def is_authenticated(request: Request, db: Session):
+        
+    try:    
+        token = request.headers.get("authorization")
+        if not token:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED, detail="You are Unauthorized"
+            )
+
+        token = token.split(" ")[-1]
+
+        data = jwt.decode(token, settings.SECRET_KEY, settings.ALGORITHM)
+
+        user_id = data.get("_id")
+
+        is_user = db.query(UserModel).filter(UserModel.id == user_id).first()
+
+        if not is_user:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED, detail="You are Unauthorized"
+            )
+
+        return is_user
+    except InvalidTokenError:
+        raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED, detail="You are Unauthorized"
+            )
